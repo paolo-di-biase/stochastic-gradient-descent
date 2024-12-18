b6fcb9575a6dfcf3f290c04cb03daf23cc2cc120
Paolo Di Biase <paolodibiase1@gmail.com>
2024-12-18 17:54:28 +0100

    added imports

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from utils.plotting import *
